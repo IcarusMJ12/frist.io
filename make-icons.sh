85ebdde81b99a329a921f961130b5c96d330e68d
Icarus <icarus.aquinas.mj12@gmail.com>
2024-04-03 16:35:52 -0700

    ch-ch-ch-ch-changeees

--- make-icons.sh
+++ make-icons.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for i in 128 48 32 16; do
-  convert -background none -density 400 forte.svg -resize ${i}x${i} icons/forte-${i}.png
+  convert -background none veresk.png -resize ${i}x${i} icons/veresk-${i}.png
 done
