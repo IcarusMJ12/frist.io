048c28efe1da33f2cea1067116a9f78878822440
Icarus <icarus.aquinas.mj12@gmail.com>
2024-04-04 13:36:11 -0700

    veresk it is

--- make-icons.sh
+++ make-icons.sh
@@ -2,4 +2,6 @@
 
 for i in 128 48 32 16; do
   convert -background none veresk.png -resize ${i}x${i} icons/veresk-${i}.png
+  # convert -background none forte.png -resize ${i}x${i} icons/forte-${i}.png
+  # convert Jati-Matic.png -thumbnail ${i}x${i}\> -gravity center -background transparent -extent ${i}x${i} icons/jati-${i}.png
 done
