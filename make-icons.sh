b45478e25b6e9886fea1c7b242f452107e86ead3
Icarus <icarus.aquinas.mj12@gmail.com>
2024-04-06 18:01:16 -0700

    new logo, no more pretending to be livescript

--- make-icons.sh
+++ make-icons.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for i in 128 48 32 16; do
-  convert -background none veresk.png -resize ${i}x${i} icons/veresk-${i}.png
-  # convert -background none forte.png -resize ${i}x${i} icons/forte-${i}.png
+  # convert -background none veresk.png -resize ${i}x${i} icons/veresk-${i}.png
+  convert -background none forte.png -resize ${i}x${i} icons/forte-${i}.png
   # convert Jati-Matic.png -thumbnail ${i}x${i}\> -gravity center -background transparent -extent ${i}x${i} icons/jati-${i}.png
 done
